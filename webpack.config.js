07bdbd9eb861e1b32677546358e5ff58eeee049e
IU <iumansky@yandex.ru>
2020-08-28 02:50:20 +0100

    5th almost there. Initial cards displayed without like active and without trash logo.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const WebpackMd5Hash = require('webpack-md5-hash')
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-const isDev = process.env.NODE_ENV === 'development'
-const isProd = !isDev
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+new webpack.DefinePlugin({
+    'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+})
+
 
 module.exports = {
     entry: path.resolve(__dirname, 'src/components/script.js'),
@@ -21,8 +28,8 @@ module.exports = {
                 exclude: /node_modules/ // исключает папку node_modules
             },
             {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+                test: /\.css$/i,
+                use: [(isDev ? 'style-loader' : MiniCssExtractPlugin.loader), 'css-loader', 'postcss-loader']
             },
             {
                 test: /\.(woff|woff2|ttf)$/,
@@ -35,7 +42,13 @@ module.exports = {
             },
             {
                 test: /\.(jpg|jpeg|png|svg|webp)$/,
-                use: 'file-loader?name=./images/[name].[ext]&esModule=false'
+                use: [
+                    'file-loader?name=./images/[name].[ext]&esModule=false',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {}
+                    },
+                ]
             }
         ]
     },
@@ -48,5 +61,15 @@ module.exports = {
             filename: '[name].[contenthash].css'
         }),
         new WebpackMd5Hash(),
+        new OptimizeCssAssetsPlugin ({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+                    preset: ['default'],
+            },
+            canPrint: true
+
+
+        })
     ]
 };
