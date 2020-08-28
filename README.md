0d2d04d8535b5bd2f9ea106721ad53f23ab29c65
IU <iumansky@yandex.ru>
2020-08-28 17:18:27 +0100

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-# sprint_11
-Sprint 11 Mesto project
-iu2019/github.io/sprint_11
-v. 0.0.1
-
-
--- src/components/renderLoading.js
+++
@@ -1,8 +0,0 @@
-export const renderLoading = (isLoading, popup, normalText)=> {
-    if (isLoading) {
-        popup.popupEl.querySelector('.popup__button').textContent = "Загрузка"
-    }
-    else {
-        popup.popupEl.querySelector('.popup__button').textContent = normalText
-    }
-  }
